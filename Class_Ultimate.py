d970e7c5de8cce2c56b7ea3a65eb88734654eea3
wwsciffsww <76759811+wwsciffsww@users.noreply.github.com>
2020-12-31 18:01:47 -0500

    Update Class_Ultimate.py

--- Class_Ultimate.py
+++ Class_Ultimate.py
@@ -1,55 +1,84 @@
-Class Ultimate:
+class Ultimate:
 
-	def __init__(self, b1, b2, b3, b4, b5, b6, b7, b8, b9):
+	def __init__(self):
 		
-		self.board = {{b1, b2, b3}, {b4, b5, b6}, {b7, b8, b9}}
+		board = []
+
+		for i in range(3):
+
+			board.append([])
+
+			for j in range(3):
+
+				board[i].append(TicTacToe())
+
 
 	def MakeGlobalMove(player, global_row, global_col, local_row, local_col):
 
-		if board[global_row][global_col].CheckLocalState() == 0:
+		if board[global_row][global_col].CheckLocalState() == None:
 
 			board[global_row][global_col] = board[global_row][global_col].MakeLocalMove(player, local_row, local_col)
 
-	def CheckRowWin(player):
+	def CheckRowWin():
 		
 		for i in range(3):
 			
-			if board[i][1] == board[i][2] == board[i][3]:
+			if board[i][0].CheckLocalState() == board[i][1].CheckLocalState() == board[i][2].CheckLocalState() == 1:
 				
-				return True
-		
-		return False
+				return 1
 
-	def CheckColWin(player):
+			if board[i][0].CheckLocalState() == board[i][1].CheckLocalState() == board[i][2].CheckLocalState() == -1:
+				
+				return -1
+
+			if board[i][0].CheckLocalState() != None and board[i][1].CheckLocalState() != None and board[i][2].CheckLocalState() != None:
+
+				return 0
+
+		return None
+		
+
+	def CheckColWin():
 		
 		for j in range(3):
 			
-			if  board[1][j] == board[2][j] == board[3][j]:
+			if 1 == board[0][j].CheckLocalState() == board[1][j].CheckLocalState() == board[2][j].CheckLocalState():
 				
-				return True
-		
-		return False
-
-	def CheckDiagWin(player):
-		
-		if board[1][1] == board[2][2] == board[3][3]:
+				return 1
+			
+			if -1 == board[0][j].CheckLocalState() == board[1][j].CheckLocalState() == board[2][j].CheckLocalState():
 				
-			return True
+				return -1
 
-		if board[3][1] == board[2][2] == board[1][3]:
+	def CheckDiagWin():
+
+		#top left to bottom right
+		if 1 == board[0][0].CheckLocalState() == board[1][1].CheckLocalState() == board[2][2].CheckLocalState():
 				
-			return True
+			return 1
 
-		return False
+		if -1 == board[0][0].CheckLocalState() == board[1][1].CheckLocalState() == board[2][2].CheckLocalState():
+				
+			return -1
+
+		#top right to bottom left
+		if 1 == board[2][0].CheckLocalState() == board[1][1].CheckLocalState() == board[0][2].CheckLocalState():
+				
+			return 1
+
+		if -1 == board[2][0].CheckLocalState() == board[1][1].CheckLocalState() == board[0][2].CheckLocalState():
+				
+			return -1
+
 
 	def CheckGlobalState():
 
-		if CheckRowWin(1) or CheckColWin(1) or CheckDiagWin(1):
+		if CheckRowWin() == 1 or CheckColWin() == 1 or CheckDiagWin() == 1:
 
 			return 1
 
-		if CheckRowWin(-1) or CheckColWin(-1) or CheckDiagWin(-1):
+		if CheckRowWin() == -1 or CheckColWin() == -1 or CheckDiagWin() == -1:
 
 			return -1
 
-		return 0
+		return None
