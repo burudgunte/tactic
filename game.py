be4641a74ec654ba263f5ed02525aa726f5fd9ae
Arnav Burudgunte <ab141@rice.edu>
2020-12-31 18:20:20 -0600

    Write Game.play() method

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import json
-from ultimate import Ultimate
+from Class_Ultimate import Ultimate
 
 class Game:
 
@@ -18,7 +18,7 @@ class Game:
         return self.pointed_to
 
     def set_board(self, newboard):
-        pass
+        self.board = newboard
 
     def set_player(self, newplayer):
         self.player = newplayer
@@ -27,9 +27,35 @@ class Game:
         self.pointed_to = (row, col)
 
     def to_json(self):
-        state = {"board":self.get_board, "player":self.get_player,
-                 "pointed_to":self.pointed_to}
+        state = {"board":self.get_board(), "player":self.get_player(),
+                 "pointed_to":self.pointed_to()}
         return json.dumps(state)
 
     def play(self):
-        pass
+        while not self.board.check_global_state():
+
+            # Choose a  board
+            if not self.get_pointed_to():
+                global_row = int(input("Row for global board: "))
+                global_col = int(input("Column for global board: "))
+            else:
+                global_row, global_col = self.get_pointed_to()
+
+            # Prompt for move
+            local_row = int(input("Row for move: "))
+            local_col = int(input("Column for move: "))
+            newboard = self.board.make_global_move(self.get_player, global_row,
+                                                     global_col, local_row, local_col)
+            self.set_board(newboard)
+
+            # Choose the board for the next move
+            if not self.board[local_row][local_col].check_local_state():
+                self.set_pointed_to(local_row, local_col)
+            else:
+                pointed_to = None
+
+            # Switch player
+            self.set_player(-1 * self.get_player())
+
+        winner = self.board.check_global_state()
+        print("{} wins the game".format(winner))
