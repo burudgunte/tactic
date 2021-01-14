0a96b84a06ced62b11020fdd9f15f449d02f8c9d
Arnav Burudgunte <ab141@rice.edu>
2021-01-13 18:51:38 -0600

    Don't color filled-in squares

--- square.js
+++ square.js
@@ -26,14 +26,16 @@ export default class Square {
         return new Square(player);
     }
 
-    draw(ctx, xSquare, ySquare, squareSize, color = "rgb(57, 57, 57)") {
+    draw(ctx, xSquare, ySquare, squareSize, color = "rgb(44, 44, 44)") {
         // Draw square
         ctx.strokeRect(xSquare, ySquare, squareSize, squareSize);
         let symbol = this.stateToSymbol();
 
         // Color based on validity
-        ctx.fillStyle = color;
-        ctx.fillRect(xSquare, ySquare, squareSize, squareSize);
+        if (!symbol) {
+            ctx.fillStyle = color;
+            ctx.fillRect(xSquare, ySquare, squareSize, squareSize);
+        }
 
         // Draw symbol
         ctx.textAlign = "center";
