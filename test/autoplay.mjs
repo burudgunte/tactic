855e529dd6449f89ecc3d4603ada9b324f57ecb7
Arnav Burudgunte <ab141@rice.edu>
2021-01-18 20:07:05 -0600

    Add alpha-beta testing job

--- test/autoplay.mjs
+++ test/autoplay.mjs
@@ -6,6 +6,7 @@
 import GlobalGame from "../game/globalgame.js";
 import randomMove from "../algorithm/random.js";
 import minimaxSearch from "../algorithm/minimax.js";
+import alphaBetaSearch from "../algorithm/alphabeta.js";
 import beamSearch from "../algorithm/beam.js";
 
 function playGame(p1Algorithm, p2Algorithm) {
