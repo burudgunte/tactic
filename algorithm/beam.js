091e1083041cec4bbcbd97847460639ee8419b02
Arnav Burudgunte <ab141@rice.edu>
2021-01-18 19:47:33 -0600

    Fix beam search move ordering function

--- algorithm/beam.js
+++ algorithm/beam.js
@@ -2,21 +2,28 @@
     
 import heuristicA from "./heuristics.js";
 
-function sortByHeuristic(possibleMoves, heuristic = heuristicA) {
+function sortByHeuristic(game, possibleMoves, heuristic = heuristicA) {
+    // Sorts an array of possible moves by heuristic
     const sortedMoves = possibleMoves.slice().sort(function (moveA, moveB) {
-        return heuristic(moveA) - heuristic(moveB);
+        const gameA = game.makeGlobalMove(moveA.localRow, moveA.localCol, 
+                        moveA.globalRow, moveA.globalCol);
+        const gameB = game.makeGlobalMove(moveB.localRow, moveB.localCol, 
+                        moveA.globalRow, moveB.globalCol)
+        return heuristic(gameA) - heuristic(gameB);
     });
     return sortedMoves;
 }
 
 function maxValue(game, depth, beta) {
-    if (game.checkGlobalState() === null || depth === 0) {
+    if (game.checkGlobalState() !== null || depth === 0) {
         return [heuristicA(game), null];
     }
     let maxUtility = Number.NEGATIVE_INFINITY;
-    let possibleMoves = sortbyHeuristic(game.getValidMoves()).slice(beta + 1);
+    let possibleMoves = sortByHeuristic(game, game.getValidMoves()).slice(0, beta + 1);
+    // console.log(game.getValidMoves().length + " valid moves");
+    // console.log(possibleMoves.length + " moves being considered")
     for (const possibleMove of possibleMoves) {
-        let newGame = game.MakeGlobalMove(possibleMove.localRow, possibleMove.localCol, possibleMove,globalRow, possibleMove.globalCol);
+        let newGame = game.makeGlobalMove(possibleMove.localRow, possibleMove.localCol,                                     possibleMove.globalRow, possibleMove.globalCol);
         let newUtility = minValue(newGame, depth - 1, beta)[0];
 
         if (newUtility > maxUtility) {
@@ -28,13 +35,13 @@ function maxValue(game, depth, beta) {
 }
 
 function minValue(game, depth, beta) {
-    if (game.checkGlobalState() === null || depth === 0) {
+    if (game.checkGlobalState() !== null || depth === 0) {
         return [heuristicA(game), null];
     }
     let maxUtility = Number.POSITIVE_INFINITY;
-    let possibleMoves = sortbyHeuristic(game.getValidMoves()).slice(beta + 1);
+    let possibleMoves = sortByHeuristic(game, game.getValidMoves()).slice(0, beta + 1);
     for (const possibleMove of possibleMoves) {
-        let newGame = game.MakeGlobalMove(possibleMove.localRow, possibleMove.localCol, possibleMove,globalRow, possibleMove.globalCol);
+        let newGame = game.makeGlobalMove(possibleMove.localRow, possibleMove.localCol,                                     possibleMove.globalRow, possibleMove.globalCol);
         let newUtility = minValue(newGame, depth - 1, beta)[0];
 
         if (newUtility < maxUtility) {
@@ -45,7 +52,11 @@ function minValue(game, depth, beta) {
     return [maxUtility, bestMove];
 }
 
-export default function beamSearch(game, beta = 15, depth = 3) {
-    let bestMove = maxValue(game, beta, depth);
+export default function beamSearch(game, depth = 4, beta = 5) {
+    if (game.player === 1) {
+        var bestMove = maxValue(game, depth, beta)[1];
+    } else if (game.player === -1) {
+        var bestMove = minValue(game, depth, beta)[1];
+    }
     return bestMove;
 }
