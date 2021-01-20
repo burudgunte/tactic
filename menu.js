de263e26ba7ab27e48b33db07a8d7e76d284c582
Arnav Burudgunte <ab141@rice.edu>
2021-01-19 20:03:09 -0600

    Test deeper variations of beam and minimax

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 import startGame from "./game/main.js";
 import randomMove from "./algorithm/random.js";
 import minimaxSearch from "./algorithm/minimax.js";
+import alphaBetaSearch from "./algorithm/alphabeta.js";
 
 document.getElementById("newgame").addEventListener("click", storeInfo);
 document.getElementById("p1").addEventListener("change", toggleOptions1);
@@ -56,5 +57,5 @@ function storeInfo() {
   let player2 = document.getElementById("p2").value;
   let timectrl2 = document.getElementById("tc2").value;
 
-  startGame(randomMove, minimaxSearch);
+  startGame(randomMove, alphaBetaSearch);
 }
