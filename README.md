648a984af7ee6921f8b3c0cdaf6531352e2aba51
Arnav Burudgunte <ab141@rice.edu>
2021-01-28 18:11:34 -0600

    Automate win rate plotting

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-## TACTIC: An Ultimate Tic-Tac-Toe Player
+# TACTIC
 
-Work in progress.
+An Ultimate Tic-Tac-Toe Player
+
+[Play the game](https://arnav-b.github.io/tactic/)
+
+### Win Tracker
+
+In development, we tested TACTIC against randomly-generated moves.
+As we collect data, we'll update the tracker with the algorithm's performance
+against human opponents.
+
+![TACTIC Win Rate vs Random](./data/minimax_vs_random.png)
--- test/probcut.R
+++ test/probcut.R
@@ -1,32 +1,16 @@
-library(tidyverse)
-library(lubridate)
-library(rjson)
-
-# Win rate vs random over time
-alphabeta <- read_csv("./data/alphaBetaSearch.csv")
-alphabeta$Time <- ymd_hms(alphabeta$Time)
-
-ggplot(data = alphabeta) +
-    geom_point(mapping = aes(x = Time, y = Win))
-
-################################################################
-# PROBCUT Stats
-################################################################
-
-
 # Linear regression for depth 4 vs depth 9, 25 moves in
+
 probcut <- read_csv("./depth25.csv") 
 probcut$Depth5 <- probcut$`5`
 probcut$Depth8 <- probcut$`8`
 
 probcut_mod <- lm(Depth8 ~ Depth5, data = probcut)
-print(summary(probcut_mod))
 
+png(file = "probcut.png")
 ggplot(data = probcut, mapping = aes(x = Depth5, y = Depth8)) + 
     xlab("Heuristic Value at Depth 5") +
     ylab("Heuristic Value at Depth 8") +
     ggtitle("Deep vs Shallow Heuristic Values") +
     geom_point() +
     stat_smooth(method = "lm", col = "blue")
-
-qqline(rstandard(probcut_mod))
+dev.off()
